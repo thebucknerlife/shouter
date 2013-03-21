ea2a4faa901707fc95a5d3fae5b19e8e127547b3
Greg Buckner <greg@theraftapp.com>
2013-03-20 21:52:39 -0700

    Initial Commit

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,4 @@
+class ShoutsController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = 'a8fecaa548797bf095fec2a586c05796c4122a79842d48bdad62a571c5d57cf3e4174baad83ed8343e541d43382b81c35f8e572a7ae343c39d16c34a8717f109'
